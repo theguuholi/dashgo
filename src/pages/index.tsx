200f8c4c6abd7b8c12c0af7846ec60bc394d4393
Gustavo Oliveira <g.92oliveira@gmail.com>
2023-03-17 08:25:00 -0300

    sigin form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,52 @@
+import { Button, Flex, FormControl, FormLabel, Input, Stack } from "@chakra-ui/react";
+
 export default function Home() {
-  return (<h1>Hello dash</h1>)
+  return (
+    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        background="gray.800"
+        borderRadius={8}
+        flexDir="column"
+        p="8"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              name="email"
+              id="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{ bgColor: "gray.900" }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{ bgColor: "gray.900" }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
