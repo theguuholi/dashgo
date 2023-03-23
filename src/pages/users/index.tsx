ec228ea1610948b6c2507ca2e4a3d70319e1b3e0
Gustavo Oliveira <g.92oliveira@gmail.com>
2023-03-23 07:58:21 -0300

    fix query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -38,7 +38,7 @@ export default function UserList() {
 
   async function handlePrefetchUser(userId: Number){
     return await queryClient.prefetchQuery(["user", userId], async () => {
-      const response = await api.get("users/${userId}")
+      const response = await api.get(`users/${userId}`)
       return response.data
     }, {
       staleTime: 1000 * 60 * 10 //10 minutos
